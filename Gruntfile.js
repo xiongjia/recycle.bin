d92f2657a730da8fe9f0d6758046a06af70d2e66
xiongjia <lexiongjia@gmail.com>
2014-02-03 03:29:46 +0800

    add sitemap, add more wiki

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var util = require('util');
+var util = require('util'),
+  _ = require('underscore');
 
 module.exports = function (grunt) {
   var config;
@@ -24,7 +25,7 @@ module.exports = function (grunt) {
     jshint: {
       options: { jshintrc: '.jshintrc' },
       gruntfile: { src: 'Gruntfile.js' },
-      config: { src: ['_config/vimwiki.js'] }
+      config: { src: ['Gruntfile.js', '_config/vimwiki.js'] }
     },
     concat: {
       css: {
@@ -66,6 +67,11 @@ module.exports = function (grunt) {
         dest: 'dist/'
       }
     },
+    sitemap: {
+      cwd: 'dist',
+      dest: 'dist/sitemap.txt',
+      site: 'http://test.com',
+    },
     clean: ['dist']
   });
 
@@ -77,11 +83,33 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  /* register tasks */
+  /* sitemap task */
+  grunt.registerTask('sitemap', 'create sitemap.txt', function () {
+    var pages, pgCwd, dest, site, data;
+    grunt.config.requires('sitemap', 'sitemap.cwd',
+      'sitemap.dest', 'sitemap.site');
+
+    pgCwd = grunt.config.get('sitemap.cwd');
+    dest = grunt.config.get('sitemap.dest');
+    site = grunt.config.get('sitemap.site');
+    grunt.log.writeln('creating sitemap. { cwd:%s, dest: %s, site: %s }',
+      pgCwd, dest, site);
+
+    pages = grunt.file.expand({cwd: pgCwd}, '**/*.html');
+    data = '';
+    _.each(pages, function (pg) {
+      data = data + util.format('%s/%s\n', site, pg);
+    });
+    grunt.file.write(dest, data);
+  });
+
+  /* alias */
   grunt.registerTask('dist',
-    ['clean', 'jshint', 'concat', 'cssmin', 'uglify', 'copy' ]);
+    'Export all files to dist folder',
+    ['clean', 'jshint', 'concat', 'cssmin', 'uglify', 'copy', 'sitemap']);
   grunt.registerTask('dist-debug',
-    ['clean', 'jshint', 'concat', 'copy' ]);
+    'Export all files to dist folder (disable uglify)',
+    ['clean', 'jshint', 'concat', 'copy', 'sitemap']);
 
   /* default task */
   grunt.registerTask('default', ['dist']);
