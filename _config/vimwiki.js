fb98f769a78adb0b7415c053cc4bdb31489deee9
xiongjia <lexiongjia@gmail.com>
2014-02-06 04:14:01 +0800

    add tips wiki

--- _config/vimwiki.js
+++ _config/vimwiki.js
@@ -9,7 +9,7 @@
     toc = $('.toc');
     toc.wrap('<div class="vw-toc">');
     $('.vw-toc').prepend(tocToggler)
-      .delay(300)
+      .delay(100)
       .fadeTo(500, '0.5')
       .hover(function() {
         $(this).stop().fadeTo(300, '0.9');
@@ -20,6 +20,13 @@
     tocToggler.click(function() {
         $('div.toc').slideToggle(300);
       });
+
+    /* update external links */
+    $('a[href]').each(function() {
+      if (this.href.indexOf(window.location.host) === -1) {
+        $(this).attr({ target: '_blank', title: this.href });
+      }
+    });
 
     /* update syntax */
     SyntaxHighlighter.all();
