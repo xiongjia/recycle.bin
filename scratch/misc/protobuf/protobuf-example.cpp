8f532713abd543a3221423ee65a07752bfede37b
xiongjia <lexiongjia@gmail.com>
2022-11-27 21:43:29 +0800

    adding proto buf example

---
+++ scratch/misc/protobuf/protobuf-example.cpp
@@ -0,0 +1,12 @@
+/**
+ * protobuf example
+ */
+
+#include <google/protobuf/util/time_util.h>
+#include "addressbook.pb.h"
+
+int main(int argc, char **argv) {
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  return 0;
+}
