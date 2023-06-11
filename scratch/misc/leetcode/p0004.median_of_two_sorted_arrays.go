6a54b20b82c492f2ddcbb4f43ccf43de987e2c03
xiongjia <lexiongjia@gmail.com>
2023-06-11 18:47:43 +0800

    updating leetcode

--- scratch/misc/leetcode/p0004.median_of_two_sorted_arrays.go
+++ scratch/misc/leetcode/p0004.median_of_two_sorted_arrays.go
@@ -1,10 +1,58 @@
 package leetcode
 
 func FindMedianSortedArraysSolution1(nums1 []int, nums2 []int) float64 {
-	if len(nums1) > len(nums2) {
-		return FindMedianSortedArraysSolution1(nums2, nums1)
+	n1Len, n2Len := len(nums1), len(nums2)
+	if n1Len > n2Len {
+		nums1, nums2 = nums2, nums1
+		n1Len, n2Len = n2Len, n1Len
 	}
 
-	// TODO
-	return 0.0
+	// totalLeft
+	//  1. n1Mid + n2Mid
+	//  2. (len(nums1) + len(nums2)) / 2
+	totalLeft := (n1Len + n2Len + 1) >> 1
+	n1Mid, n2Mid := 0, 0
+	n1Low, n1High := 0, n1Len
+	for n1Low <= n1High {
+		n1Mid = n1Low + (n1High-n1Low)>>1
+		n2Mid = totalLeft - n1Mid
+		if n1Mid > 0 && nums1[n1Mid-1] > nums2[n2Mid] {
+			n1High = n1Mid - 1
+		} else if n1Mid != n1Len && nums1[n1Mid] < nums2[n2Mid-1] {
+			n1Low = n1Mid + 1
+		} else {
+			break
+		}
+	}
+
+	totalMidLeft, totalMidRight := 0, 0
+	if n1Mid == 0 {
+		totalMidLeft = nums2[n2Mid-1]
+	} else if n2Mid == 0 {
+		totalMidLeft = nums1[n1Mid-1]
+	} else {
+		if nums1[n1Mid-1] > nums2[n2Mid-1] {
+			totalMidLeft = nums1[n1Mid-1]
+		} else {
+			totalMidLeft = nums2[n2Mid-1]
+		}
+	}
+
+	if (n1Len+n2Len)&1 == 1 {
+		return float64(totalMidLeft)
+	}
+
+	if n1Mid == n1Len {
+		totalMidRight = nums2[n2Mid]
+	} else if n2Mid == n2Len {
+		totalMidRight = nums1[n1Mid]
+	} else {
+		if nums1[n1Mid] < nums2[n2Mid] {
+			totalMidRight = nums1[n1Mid]
+		} else {
+			totalMidRight = nums2[n2Mid]
+		}
+	}
+
+	return float64(totalMidLeft+totalMidRight) / 2
 }
--- scratch/misc/leetcode/p0004.median_of_two_sorted_arrays_test.go
+++ scratch/misc/leetcode/p0004.median_of_two_sorted_arrays_test.go
@@ -1,7 +1,30 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_p0004(t *testing.T) {
-	// TODO
+	cases := []struct {
+		n1 []int
+		n2 []int
+		e  float64
+	}{
+		{
+			n1: []int{4, 5, 6, 8, 9}, n2: []int{}, e: 6.0,
+		}, {
+			n1: []int{1, 3}, n2: []int{2}, e: 2.0,
+		}, {
+			n1: []int{1, 2}, n2: []int{3, 4}, e: 2.5,
+		}, {
+			n1: []int{1, 2, 3}, n2: []int{1, 2}, e: 2.0,
+		},
+	}
+	for _, c := range cases {
+		output := FindMedianSortedArraysSolution1(c.n1, c.n2)
+		t.Logf("n1 = %v, n2 = %v, e = %v; o = %v\n", c.n1, c.n2, c.e, output)
+		assert.Equal(t, output, c.e)
+	}
 }
