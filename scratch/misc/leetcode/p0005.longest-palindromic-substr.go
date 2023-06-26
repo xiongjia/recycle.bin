d1f1370fb86c525fe4f5b4bdd9f333d43d4baabd
xiongjia <lexiongjia@gmail.com>
2023-06-27 00:29:36 +0800

    updating problem tests

--- scratch/misc/leetcode/p0005.longest-palindromic-substr.go
+++ scratch/misc/leetcode/p0005.longest-palindromic-substr.go
@@ -1,6 +1,17 @@
 package leetcode
 
 func longestPalindromeSlution1(s string) string {
-	// TODO
-	return "aba"
+	res, dp := "", make([][]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]bool, len(s))
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := i; j < len(s); j++ {
+			dp[i][j] = (s[i] == s[j]) && ((j-i < 3) || dp[i+1][j-1])
+			if dp[i][j] && (res == "" || j-i+1 > len(res)) {
+				res = s[i : j+1]
+			}
+		}
+	}
+	return res
 }
